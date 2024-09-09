2e8121e60f6714ac1850f4339dffb31e65ed93c0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-09 16:30:43 +0600

    data types in php

---
+++ 4-data-types/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$age = 23;
+$name = "Mugdho";
+$married = false;
+$pi = 3.1416;
+
+echo var_dump($age) . "<br>";
+echo var_dump($name) . "<br>";
+echo var_dump($married) . "<br>";
+echo var_dump($pi) . "<br>";
+
+var_dump($pi);
+echo "<br>";
+
+var_dump($unknown);
+
+?>
