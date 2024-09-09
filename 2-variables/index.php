1794ea468e3431f1b28b23ef8b38d83f9a9eac88
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-09 16:10:21 +0600

    format

--- 2-variables/index.php
+++ 2-variables/index.php
@@ -1,6 +1,6 @@
 <?php
- $name = "M. Oly Mahmud";
- echo "My name is $name";
- $sports = ["cricket","football"];
- echo "My favorite sport is " . $sports[0] . " but I also like $sports[1]";
+$name = "M. Oly Mahmud";
+echo "My name is $name";
+$sports = ["cricket", "football"];
+echo "My favorite sport is " . $sports[0] . " but I also like $sports[1]";
 ?>
