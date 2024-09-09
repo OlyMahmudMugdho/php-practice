7152a9cbda17dd4e77b719b907e475de404e9289
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-09 16:45:04 +0600

    array

---
+++ 5-arrays/index.php
@@ -0,0 +1,14 @@
+<?php
+ $nums = [19,46,35,68,5,645,4];
+
+ for ($i=0; $i < count($nums); $i++) { 
+    echo "$i <br>";
+ }
+
+ $fruits = Array("apple", "banana", "mango");
+
+ for ($i= 0; $i < count($fruits); $i++) {
+    echo $fruits[$i] ." ";
+ }
+
+?>
