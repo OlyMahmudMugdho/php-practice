9522feb24cb7e3a4f11dfea28333756f37085fea
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-11 20:48:17 +0600

    cookie

---
+++ 11-cookie/index.php
@@ -0,0 +1,30 @@
+<?php
+    function getName(){
+        return filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+
+    if(isset($_POST["submit"])){
+        if (!empty(getName())) {
+            setcookie("name", getName(), time() + 86400);
+        }
+        else {
+            echo "name can't be empty";
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookie</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+        <input type="text" name="name" id="name"> <br>
+        <button name="submit" type="submit">Save</button>
+    </form>
+</body>
+</html>
