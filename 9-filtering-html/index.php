47c59c1ea978996511f4d93ea3a75033d4e31690
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-11 12:42:43 +0600

    filter_input()

--- 9-filtering-html/index.php
+++ 9-filtering-html/index.php
@@ -3,6 +3,9 @@
 
     if ($submitted) {
         echo htmlspecialchars($_GET["name"]) . "<br>";
+        echo "filter_input() <br>";
+        $age = filter_input(INPUT_GET, "age",FILTER_SANITIZE_SPECIAL_CHARS);
+        echo $age . "<br>";
     }
 ?>
 
@@ -16,7 +19,10 @@
 </head>
 <body>
     <form action="<?php echo $_SERVER["PHP_SELF"] ?>">
+        <label for="name">Name : </label> <br>
         <input type="text" name="name" id="name"> <br>
+        <label for="age">Age : </label> <br>
+        <input type="text" name="age" id="age"> <br>
         <button type="submit">Send Data</button>
     </form>
 </body>
