5eea29511e86fc83cfb6952dd2a33db7e7e1b323
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-11 12:46:19 +0600

    post data

---
+++ 10-post-request/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sanitize</title>
+</head>
+<body>
+    <form method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+        <label for="name">Name : </label> <br>
+        <input type="text" name="name" id="name"> <br>
+        <label for="age">Age : </label> <br>
+        <input type="text" name="age" id="age"> <br>
+        <button type="submit">Send Data</button>
+    </form>
+</body>
+</html>
+
+
+
+<?php
+    $submitted = isset($_POST["name"]);
+
+    if ($submitted) {
+        echo htmlspecialchars($_POST["name"]) . "<br>";
+        $age = filter_input(INPUT_POST, "age",FILTER_SANITIZE_SPECIAL_CHARS);
+        echo $age . "<br>";
+    }
+?>
